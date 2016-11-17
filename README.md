134188e9a99a7555d78f53af81de934bd0ceb9f5
Alexander Rodionov <alx1dr@gmail.com>
2016-11-17 15:56:40 +0300

    forked this project from my coursework repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Realtime Microphone Visualisation with NAudio
+
+This project simply captures audio from your microphone and draw it on Windows Form chart
+
+how it looks
+![scren](screenshot.png)
---
+++ naudio-mic-realtime-visualisation/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using NAudio.Wave;
+using NAudio;
+
+namespace naudio_mic_realtime_visualisation
+{
+    public partial class Form1 : Form
+    {
+        int n = 4000; // number of x-axis pints
+        //Stopwatch time = new Stopwatch();
+        WaveIn wi;
+        Queue<double> myQ;
+        public Form1()
+        {
+            InitializeComponent();
+            myQ = new Queue<double>(Enumerable.Repeat(0.0, n).ToList()); // fill myQ w/ zeros
+            chart1.ChartAreas[0].AxisY.Minimum = -10000;
+            chart1.ChartAreas[0].AxisY.Maximum = 10000;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wi = new WaveIn();
+            wi.StartRecording();
+            wi.WaveFormat = new WaveFormat(4, 16, 1); // (44100, 16, 1);
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+            timer1.Enabled = true;
+            //time.Start();
+        }
+
+
+        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            for (int i = 0; i < e.BytesRecorded; i += 2)
+            {
+                myQ.Enqueue(BitConverter.ToInt16(e.Buffer, i));
+                myQ.Dequeue();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                chart1.Series["Series1"].Points.DataBindY(myQ);
+                //chart1.ResetAutoValues();
+            }
+            catch
+            {
+                Console.WriteLine("No bytes recorded");
+            }
+        }
+    }
+}
